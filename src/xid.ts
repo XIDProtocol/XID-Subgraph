b78da7e9769cc7813d55aad0a326a14d415ad92e
Jack Lee <280147597@qq.com>
2024-09-10 19:52:05 +0800

    update xid.ts and schema

--- src/xid.ts
+++ src/xid.ts
@@ -4,25 +4,22 @@ import { store, BigInt } from "@graphprotocol/graph-ts"
 import { XID } from "../generated/XID/XID"
 
 export function handleMint(event: Mint): void {
-  let user = User.load(event.params.user.toHex())
+  let user = User.load(event.params.user.toHexString())
   if (user == null) {
-    user = new User(event.params.user.toHex())
+    user = new User(event.params.user.toHexString())
     user.address = event.params.user
-  } 
+  }
   user.username = event.params.username
 
-  let xidToken = new XIDToken(event.params.tokenId.toHex())
+  let xidToken = new XIDToken(event.params.tokenId.toHexString())
   xidToken.tokenId = event.params.tokenId
   xidToken.owner = user.id
   xidToken.username = event.params.username
   xidToken.expirationTime = event.params.expirationTime
-  xidToken.isValid = true
 
   user.xidToken = xidToken.id
   user.save()
   xidToken.save()
-
-  updateGlobalState(event.block.timestamp)
 }
 
 export function handleBurn(event: Burn): void {
@@ -35,48 +32,13 @@ export function handleBurn(event: Burn): void {
     store.remove('XIDToken', xidToken.id)
   }
 
-  updateGlobalState(event.block.timestamp)
 }
 
 export function handleRegistrationRenewed(event: RegistrationRenewed): void {
-  let xidToken = XIDToken.load(event.params.tokenId.toHex())
+  let xidToken = XIDToken.load(event.params.tokenId.toHexString())
   if (xidToken != null) {
     xidToken.expirationTime = event.params.newExpirationTime
-    xidToken.isValid = true
     xidToken.save()
   }
-
-  updateGlobalState(event.block.timestamp)
 }
 
-function updateGlobalState(timestamp: BigInt): void {
-  let globalState = GlobalState.load("1")
-  if (globalState == null) {
-    globalState = new GlobalState("1")
-    globalState.lastCheckTime = timestamp
-  }
-  globalState.lastCheckTime = timestamp
-  globalState.save()
-
-  checkExpiredTokens(timestamp)
-}
-
-function checkExpiredTokens(currentTime: BigInt): void {
-  let xidContract = XID.bind(XID.address)
-
-  let tokens = XIDToken.load()
-  for (let i = 0; i < tokens.length; i++) {
-    let token = tokens[i]
-    if (token.expirationTime.le(currentTime) && token.isValid) {
-      token.isValid = false
-      token.save()
-
-      let user = User.load(token.owner)
-      if (user != null) {
-        user.username = null
-        user.xidToken = null
-        user.save()
-      }
-    }
-  }
-}
